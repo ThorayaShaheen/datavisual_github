91a7339e28e7a160c417adbfc0bebd5c1785bd8d
ThorayaShaheen <shaheent@tcd.ie>
2018-11-27 14:15:11 -0800

    implementing the chart

--- js/app.js
+++ js/app.js
@@ -14,7 +14,23 @@ $(document).ready(function(){
     });
   };
 
+  // get repos
+  function getUserRepos(callback){
+    $.get("https://api.github.com/users/" + gitHubUser + "/repos", 
+      function(data, status){
+        console.log(status);
+        success: callback(data,status);
+    });
+  };
 
+  // get languages
+  function getRepoActivity(callback,repo){
+    $.get("https://api.github.com/repos/" + gitHubUser + "/" + repo + "/stats/commit_activity", 
+      function(data, status){
+          console.log(status);
+          success: callback(data,status,repo);
+    });
+  };
 
   function getRepoContributors(callback,repo){
     $.get("https://api.github.com/repos/" + repo + "/stats/contributors", 
@@ -25,6 +41,26 @@ $(document).ready(function(){
   };
   // getRepoActivity(, 'github-api-access');
 
+
+
+var data = {[]}
+function drawBarChart(data){
+  // mostly via https://gist.github.com/Jverma/887877fc5c2c2d99be10 with contextual updates
+  // set the dimensions of the canvas
+  var margin = {top: 20, right: 20, bottom: 70, left: 40},
+      width = 1200 - margin.left - margin.right,
+      height = 600 - margin.top - margin.bottom;
+
+
+  // set the ranges
+  var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+  var y = d3.scale.linear().range([height, 0]);
+
+  // define the axis
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
 
 
 
