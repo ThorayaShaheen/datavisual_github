5133a417914ce5875ea6a958bc8525ddd2d37b77
ThorayaShaheen <shaheent@tcd.ie>
2018-11-27 12:45:11 -0800

    try to connect with api

--- js/app.js
+++ js/app.js
@@ -5,4 +5,27 @@ $(document).ready(function(){
   var gitHubUser = 'elementary';
   var repo = 'music';
 
+  // get user
+  function getUserData(callback) {
+    $.get("https://api.github.com/users/" + gitHubUser, 
+      function(data, status){
+        console.log(status);
+        success: callback(data, status);
+    });
+  };
+
+
+
+  function getRepoContributors(callback,repo){
+    $.get("https://api.github.com/repos/" + repo + "/stats/contributors", 
+      function(data, status){
+          console.log(status);
+          success: callback(data,status,repo);
+    });
+  };
+  // getRepoActivity(, 'github-api-access');
+
+
+
+
 }
