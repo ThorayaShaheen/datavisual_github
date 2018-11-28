0b225cff7282780520e929788e1648e435141cf6
ThorayaShaheen <shaheent@tcd.ie>
2018-11-27 16:45:12 -0800

    extrac the data from the api and add it to chart

--- js/app.js
+++ js/app.js
@@ -43,7 +43,23 @@ $(document).ready(function(){
 
 
 
-var data = {[]}
+  getRepoActivity(function (git_data){
+    var data = [];
+    for(var i=0;i<git_data.length;i++){
+      data.push({title: "Week "+(i+1), plays: git_data[i].total},)
+    }
+    drawBarChart(data);
+  },repo)
+  getRepoContributors(function (git_data){
+    var data = []
+    for(var i=git_data.length-1;i>Math.max(-1,git_data.length-11);i--){
+      data.push({login: git_data[i].author.login, total: (git_data[i].total)},)
+    } 
+    drawBubbleChart(data);
+  },"mohelm97/music")
+
+});
+
 function drawBarChart(data){
   // mostly via https://gist.github.com/Jverma/887877fc5c2c2d99be10 with contextual updates
   // set the dimensions of the canvas
@@ -63,5 +79,51 @@ function drawBarChart(data){
       .orient("bottom")
 
 
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(d3.max(data, function(d) { return d.plays; })/*data.length*/);
+
+
+  // add the SVG element
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", 
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(data.map(function(d) { return d.title; }));
+    y.domain([0, d3.max(data, function(d) { return d.plays; })]);
+
+  // add axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 5)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
+    // Add bar chart
+    svg.selectAll("bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.title); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.plays); })
+        .attr("height", function(d) { return height - y(d.plays); });
 
 }
